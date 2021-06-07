6bd0471fa48991beeb95c58ca16bddee6c393c7c
scozdev <selcuk-ozdemir79@hotmail.com>
2021-06-07 16:14:24 +0300

    inital commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+
+const Button = styled.div``
+
+export default function Home() {
+  return <main></main>
+}
