2585e8fd101b1a24f331810eed38764eb5608ebb
scozdev <selcuk-ozdemir79@hotmail.com>
2021-06-15 16:23:48 +0300

    Add Constants

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+export const colors = ['#6D5D4B', '#3D8FE8', '#EF9636', '#4FC4A5', '#000']
+export const fonts = [
+  'Poppins',
+  'sans-serif',
+  'Nunito',
+  'Merriweather',
+  'Inconsolata',
+]
+export const fontSizes = [1, 2, 3, 4, 5]
